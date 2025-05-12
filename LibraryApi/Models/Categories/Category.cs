6d1b27055fb43be7e332b993167b5fa1726f7da1
alirezadoostdar <alireza.doostdar@gmail.com>
2025-05-12 20:04:05 +0200

    add models

---
+++ LibraryApi/Models/Categories/Category.cs
@@ -0,0 +1,10 @@
+﻿using LibraryApi.Models.Books;
+
+namespace LibraryApi.Models.Categories;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public HashSet<Book> Books { get; set; } = new HashSet<Book>();
+}
