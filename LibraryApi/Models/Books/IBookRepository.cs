094748d319ec08c1ee0d3c4a5ecf1c8fe9e812b1
alirezadoostdar <alireza.doostdar@gmail.com>
2025-05-17 21:21:50 +0200

    Add book Repository

---
+++ LibraryApi/Models/Books/IBookRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace LibraryApi.Models.Books;
+
+public interface IBookRepository
+{
+    void Add(Book book);
+}
+
+public class BookRepository : IBookRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public BookRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public void Add(Book book)
+    {
+        _context.Books.Add(book);
+        _context.SaveChanges();
+    }
+}
