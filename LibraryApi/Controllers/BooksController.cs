c83a4435f20b3175187b1f95e0081c5fce653e43
alirezadoostdar <alireza.doostdar@gmail.com>
2025-05-17 21:17:49 +0200

    add book controller

---
+++ LibraryApi/Controllers/BooksController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApi.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : Controller
+    {
+        
+    }
+}
