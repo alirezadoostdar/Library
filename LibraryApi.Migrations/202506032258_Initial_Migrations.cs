c53394b168879b983e4bb044d07fd0e914221fdd
alirezadoostdar <alireza.doostdar@gmail.com>
2025-06-03 23:15:26 +0200

    add Initial migration

---
+++ LibraryApi.Migrations/202506032258_Initial_Migrations.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Migrations
+{
+    [Migration(202506032258)]
+    public class _202506032258_Initial_Migrations : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("AgeGroups")
+                  .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                  .WithColumn("Title").AsString(50).NotNullable();
+        }
+        public override void Down()
+        {
+            Delete.Table("AgeGroups");
+        }
+
+
+    }
+}
