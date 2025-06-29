2827a5291394e9b2049da78817f52a68f48ed76c
alirezadoostdar <alireza.doostdar@gmail.com>
2025-06-29 13:59:14 +0200

    Add fluent migration for members table

---
+++ LibraryApi.Migrations/202506291348_Add_Member.cs
@@ -0,0 +1,30 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Migrations
+{
+    [Migration(202506291348)]
+    public class _202506291348_Add_Member : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Members")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Name").AsString(50).NotNullable()
+                .WithColumn("Family").AsString(50).NotNullable()
+                .WithColumn("MembershipNumber").AsInt32().NotNullable()
+                .WithColumn("MemberShipDate").AsDateTime().NotNullable()
+                .WithColumn("Address").AsString(200).Nullable()
+                .WithColumn("PhoneNumber").AsString(11).NotNullable();
+        }
+        public override void Down()
+        {
+            Delete.Table("Members");
+        }
+
+    }
+}
---
+++ LibraryApi/Controllers/MembersController.cs
@@ -0,0 +1,23 @@
+﻿using LibraryApi.Models.Members;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApi.Controllers
+{
+    [ApiController]
+    [Route("api/Members")]
+    public class MembersController : Controller
+    {
+        public readonly IMemberRepository _memberRepository;
+
+        public MembersController(IMemberRepository memberRepository)
+        {
+            _memberRepository = memberRepository;
+        }
+
+        [HttpPut]
+        public void Add(AddMemberDto dto)
+        {
+            _memberRepository.Add(dto);
+        }
+    }
+}
