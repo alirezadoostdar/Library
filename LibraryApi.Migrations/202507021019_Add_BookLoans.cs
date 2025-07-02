8dbfa055d13ff9d9828f9e0eee4569612c7c80e4
alirezadoostdar <alireza.doostdar@gmail.com>
2025-07-02 11:16:29 +0200

    Add migration for bool loans

---
+++ LibraryApi.Migrations/202507021019_Add_BookLoans.cs
@@ -0,0 +1,45 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Migrations
+{
+    [Migration(202507021019)]
+    public class _202507021019_Add_BookLoans : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("BookLoans")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("RegisterDate").AsDateTime().NotNullable()
+                .WithColumn("MustReturnDate").AsDateTime().NotNullable()
+                .WithColumn("ReturnDate").AsDateTime().Nullable()
+                .WithColumn("MemberId").AsInt32().NotNullable()
+                .WithColumn("BookId").AsInt32().NotNullable();
+
+            Create.ForeignKey("Fk_BookLoans_Members")
+                .FromTable("BookLoans")
+                .ForeignColumn("MemberId")
+                .ToTable("Members")
+                .PrimaryColumn("Id");
+
+            Create.ForeignKey("Fk_BookLoans_Book")
+                .FromTable("BookLoans")
+                .ForeignColumn("BookId")
+                .ToTable("Books")
+                .PrimaryColumn("Id");
+
+        }
+
+        public override void Down()
+        {
+            Delete.ForeignKey("Fk_BookLoans_Book");
+            Delete.ForeignKey("Fk_BookLoans_Members");
+            Delete.Table("BookLoans");
+        }
+
+    }
+}
