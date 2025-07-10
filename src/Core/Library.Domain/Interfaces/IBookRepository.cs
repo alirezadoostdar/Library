4a5b02138ff3f3fe375555c0692442659c311314
alirezadoostdar <alireza.doostdar@gmail.com>
2025-07-10 22:39:45 +0200

    create application layer

---
+++ src/Core/Library.Domain/Interfaces/IBookRepository.cs
@@ -0,0 +1,12 @@
+﻿using Library.Domain.Entities;
+
+namespace Library.Domain.Interfaces;
+
+public interface IBookRepository
+{
+    Task<Book> GetByIdAsync(int id);
+    Task<Book> AddAsync(Book book);
+    Task<Book> UpdateAsync(Book book);
+    Task DeleteAsync(int id);
+    Task<IEnumerable<Book>> GetAllAsync();
+}
