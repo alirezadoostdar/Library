6400cc06a365523338c6148659a9a7c3c9e8f2f8
alirezadoostdar <alireza.doostdar@gmail.com>
2025-07-10 22:28:19 +0200

    create entities

---
+++ src/Library.Domain/Entities/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.Domain.Entities;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+}
---
+++ src/Library.Domain/Entities/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace Library.Domain.Entities;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public ICollection<Book> Books { get; set; } = new List<Book>();
+}
