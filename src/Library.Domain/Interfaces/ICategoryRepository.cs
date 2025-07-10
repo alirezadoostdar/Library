ec0abdb116699f750b76f39388f9a6e866a40d93
alirezadoostdar <alireza.doostdar@gmail.com>
2025-07-10 22:30:36 +0200

    cteate repositry interface for category

---
+++ src/Library.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Library.Domain.Entities;
+
+namespace Library.Domain.Interfaces;
+
+public interface ICategoryRepository
+{
+    Task<Category> GetByIdAsync(int id);
+    Task<Category> AddAsync(Category category);
+    Task<Category> UpdateAsync(Category category);
+    Task DeleteAsync(int id);
+    Task<IEnumerable<Category>> GetAllAsync();
+
+}
