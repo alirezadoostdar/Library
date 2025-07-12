83e0a9f9d414d276faea9a4be8183ae0df208433
alirezadoostdar <alireza.doostdar@gmail.com>
2025-07-12 23:05:40 +0200

    add config for book

---
+++ src/Infarstructure/Library.Infarstructure/Persistence/FluentApiConfig/BookConfig.cs
@@ -0,0 +1,26 @@
+﻿using Library.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Library.Infarstructure.Persistence.FluentApiConfig;
+
+public class BookConfig : IEntityTypeConfiguration<Book>
+{
+    public void Configure(EntityTypeBuilder<Book> builder)
+    {
+        builder.ToTable("Books");
+
+        builder.HasKey(b  => b.Id);
+
+        builder.Property(b => b.Title)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(b => b.Description)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        builder.Property(b => b.CategoryId)
+            .IsRequired();
+    }
+}
