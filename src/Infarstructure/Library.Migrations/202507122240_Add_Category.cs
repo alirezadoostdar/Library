86c388d646ca1168af25139675edb2032d4ef072
alirezadoostdar <alireza.doostdar@gmail.com>
2025-07-12 22:44:33 +0200

    Add migration project

---
+++ src/Infarstructure/Library.Migrations/202507122240_Add_Category.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace Library.Migrations;
+
+[Migration(202507122240)]
+public class _202507122240_Add_Category : Migration
+{
+
+    public override void Up()
+    {
+        Create.Table("Categories")
+            .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+            .WithColumn("Title").AsString(50).NotNullable();
+    }
+    public override void Down()
+    {
+        Delete.Table("Categories");
+    }
+}
