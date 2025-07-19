c1c3d482b83a2f72fcc202ae9e3f5357872023cc
alirezadoostdar <alireza.doostdar@gmail.com>
2025-07-19 10:42:20 +0200

    create migration for add book table

---
+++ src/Infarstructure/Library.Migrations/202507191032_Add_Book.cs
@@ -0,0 +1,28 @@
+﻿using FluentMigrator;
+
+namespace Library.Migrations;
+
+[Migration(202507191032)]
+public class _202507191032_Add_Book : Migration
+{
+    public override void Up()
+    {
+        Create.Table("Books")
+            .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+            .WithColumn("Title").AsString(50).NotNullable()
+            .WithColumn("Description").AsString(200).Nullable()
+            .WithColumn("CategoryId").AsInt32().NotNullable();
+
+        Create.ForeignKey("Fk_Books_Category")
+            .FromTable("Books")
+            .ForeignColumn("CategoryId")
+            .ToTable("Categories")
+            .PrimaryColumn("Id");
+    }
+    public override void Down()
+    {
+        Delete.Table("Books");
+        Delete.ForeignKey("Fk_Books_Category");
+    }
+
+}
